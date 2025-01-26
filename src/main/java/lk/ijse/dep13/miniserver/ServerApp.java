19871fb18e8904628b4df9ad198219544c11833e
root <savindiranaweera>
2025-01-26 11:59:26 +0530

    Set up basic server to accept client connections

--- src/main/java/lk/ijse/dep13/miniserver/ServerApp.java
+++ src/main/java/lk/ijse/dep13/miniserver/ServerApp.java
@@ -1,7 +1,17 @@
 package lk.ijse.dep13.miniserver;
 
-public class ServerApp {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class ServerApp {
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(80);
+        System.out.println("Server started on port 80.");
+
+        while (true) {
+            Socket socket = serverSocket.accept();
+            System.out.println("Client connected: " + socket.getRemoteSocketAddress());
+        }
     }
 }
