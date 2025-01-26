658000450b5d39a16991cdeda30baad90834c421
root <savindiranaweera>
2025-01-26 12:09:26 +0530

    Set up input stream to read client data in ServerApp

--- src/main/java/lk/ijse/dep13/miniserver/ServerApp.java
+++ src/main/java/lk/ijse/dep13/miniserver/ServerApp.java
@@ -1,6 +1,9 @@
 package lk.ijse.dep13.miniserver;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -10,8 +13,20 @@ public class ServerApp {
         System.out.println("Server started on port 80.");
 
         while (true) {
-            Socket socket = serverSocket.accept();
-            System.out.println("Client connected: " + socket.getRemoteSocketAddress());
+            Socket localSocket = serverSocket.accept();
+            System.out.println("Client connected: " + localSocket.getRemoteSocketAddress());
+
+            new Thread(() -> {
+                try {
+                    InputStream is = localSocket.getInputStream();
+                    InputStreamReader isr = new InputStreamReader(is);
+                    BufferedReader br = new BufferedReader(isr);
+
+
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            });
         }
     }
 }
